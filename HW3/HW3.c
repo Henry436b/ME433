990e75ce6ef354785896f564cf6fd6aee04711fa
Henry Acquaye <henryacquaye2028@u.northwestern.edu>
2026-05-06 02:20:36 -0500

    Update HW3.c

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -2,30 +2,91 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+// I2C Init
 #define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define SDA_PIN 12
+#define SCL_PIN 13
 
+// MCP23008 address with A0,A1,A2 tied to GND
+#define MCP23008_ADDR 0x20
 
+// MCP23008 Registers
+#define IODIR 0x00
+#define GPIO  0x09
+#define OLAT  0x0A
+
+// HEARTBEAT LED GPIO num
+#define HEARTBEAT_LED 16
+
+//Pre call funcs
+void writeRegister(uint8_t reg, uint8_t value);
+uint8_t readRegister(uint8_t reg);
 
 int main()
 {
     stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    // Init "Heartbeat" LED
+    gpio_init(HEARTBEAT_LED);
+    gpio_set_dir(HEARTBEAT_LED, GPIO_OUT);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    // Initialize I2C
+    i2c_init(I2C_PORT, 100 * 1000);
+
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+
+    gpio_pull_up(SDA_PIN);
+    gpio_pull_up(SCL_PIN);
+
+    //GP7 output, others input
+    writeRegister(IODIR, 0x7F);
+
+    //Start LED OFF
+    writeRegister(OLAT, 0x00);
+
+    while (true){
+        // HEARTBEAT BLINK
+        gpio_put(HEARTBEAT_LED, 1);
+        sleep_ms(200);
+
+        gpio_put(HEARTBEAT_LED,0);
+        sleep_ms(100);
+
+        //Button state
+        uint8_t gpioState = readRegister(GPIO);
+
+        // Check GP0
+        bool buttonPressed = !(gpioState & 0x01);
+        // If else button
+        if (buttonPressed){
+            writeRegister(OLAT, 0x80); // Turn GP7 ON
+        }
+        else{
+            writeRegister(OLAT, 0x00); // Turn GP7 OFF
+
+        }
+        sleep_ms(100);
     }
+}
+
+// General Purpose Write Function
+void writeRegister(uint8_t reg, uint8_t value){
+    uint8_t buf[2];
+
+    buf[0] = reg;
+    buf[1] = value;
+
+    i2c_write_blocking(I2C_PORT, MCP23008_ADDR, buf, 2, false);
+}
+// General Purpose Read Function
+uint8_t readRegister(uint8_t reg){
+    uint8_t value;
+
+    // T    ell MCP23008 which register we want
+    i2c_write_blocking(I2C_PORT, MCP23008_ADDR, &reg, 1, true);
+
+    // Read the register value
+    i2c_read_blocking(I2C_PORT, MCP23008_ADDR, &value, 1, false);
+    return value;
 }
