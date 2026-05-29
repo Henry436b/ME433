d2d971ba9a01edc99ad6d47e4226e2300880b636
Henry Acquaye <henryacquaye2028@u.northwestern.edu>
2026-05-29 02:27:21 -0500

    Update HW4

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "ssd1306.h"
+
+#define LED_PIN 16
+#define I2C_SDA 12
+#define I2C_SCL 13
 
 
-int main()
-{
+int main(){
     stdio_init_all();
 
+    i2c_init(i2c_default, 400*1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    //LED Set up
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    ssd1306_setup();
+
+    int pixel_on = 0;
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        gpio_put(LED_PIN, pixel_on);
+
+        ssd1306_clear();
+        if(pixel_on){
+            ssd1306_drawPixel(64, 16, 1);
+        }
+        ssd1306_update();
+
+        pixel_on = !pixel_on;
+        sleep_ms(500);
     }
 }
